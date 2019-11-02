504f4947c0b1faef4ef004755b46cb622a6793ff
Amir Ziaee <amir.ziaee@fh-kufstein.ac.at>
2019-11-02 15:17:16 +0100

    added C++

---
+++ InitializePriorsFunction.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+
+vector<float> initialize_priors(int map_size, vector<float> landmark_positions,float position_stdev);
+int main() {
+  // set standard deviation of position
+  float position_stdev = 1.0f;
+
+  // set map horizon distance in meters
+  int map_size = 25;
+
+  // initialize landmarks
+  std::vector<float> landmark_positions {5, 10, 20};
+  // initialize priors
+  landmark_positions.push_back(30);
+  vector<float> priors = initialize_priors(map_size, landmark_positions,position_stdev);
+
+  // print values to stdout
+  for (int p = 0; p < priors.size(); ++p) {
+    std::cout << priors[p] << std::endl;
+  }
+
+  return 0;
+}
+
+vector<float> initialize_priors(int map_size, vector<float>  landmark_positions,float position_stdev)
+{
+ vector<flaot> priors(map_size,0.0);
+ float norm_term = landmark_positions.size() * (position_stdev * 2 + 1);
+  for (int i=0; i < landmark_positions.size(); ++i) {
+    for (float j=1; j <= position_stdev; ++j) {
+      priors.at(int(j+landmark_positions[i]+map_size)%map_size) += 1.0/norm_term;
+      priors.at(int(-j+landmark_positions[i]+map_size)%map_size) += 1.0/norm_term;
+    }
+    priors.at(landmark_positions[i]) += 1.0/norm_term;
+  }
+
+ return priors;
+
+}
